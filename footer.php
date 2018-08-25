25b67b4cf50ac55024bdbc797bbeaec29c8809f3
lightspeedprogramming <lightspeedprogramming@bitbucket.org>
2018-08-25 20:08:16 +0530

    Front work in progress

---
+++ footer.php
@@ -0,0 +1,92 @@
+<footer class="footer">
+		<div class="footer-widget-area s-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6">
+						<div class="f-widget about-widget">
+							<div class="footer-title">
+								<h4>About Us</h4>
+							</div>
+							<p>Proin eget tortor risus. Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6">
+						<div class="f-widget hours-widget">
+							<div class="footer-title">
+								<h4>Hours</h4>
+							</div>
+							<ul>
+								<li><span>Weekdays</span> <span class="text-right">9:00 - 17:00</span></li>
+								<li><span>Saturday</span> <span class="text-right">9:00 - 20:00</span></li>
+								<li><span>Sunday</span> <span class="text-right">Closed</span></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6">
+						<div class="f-widget newsletter-widget">
+							<div class="footer-title">
+								<h4>Newsletter</h4>
+							</div>
+							<p>Stay Updated with our news<br> We Promise not to spam!</p>
+							<form class="newslatter">
+								<input class="form-control" type="text" name="email" placeholder="Your Email">
+							</form>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6">
+						<div class="f-widget">
+							<div class="footer-title">
+								<h4>Quick Links</h4>
+							</div>
+							<ul class="f-w-list">
+								<li><a href="#">FAQ's</a></li>
+								<li><a href="#">Privacy</a></li>
+								<li><a href="#">Support Forum</a></li>
+								<li><a href="#">Themes</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="copyright-area">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-6">
+						<div class="copyright-txt text-lg-left text-center mb-3 mb-lg-0">
+							<p>&copy; Copyright 2017 by <a href="#">Iconic Theme.</a> All Right Reserved</p>
+						</div>
+					</div>
+					<div class="col-lg-6 text-lg-right text-center">
+						<div class="footer-social">
+							<ul class="social-porfiles">
+								<li><a class="facebook" href="#"><i class="fa fa-facebook-f"></i></a></li>
+								<li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a class="googleplus" href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+								<li><a class="pinterest" href="#"><i class="fa fa-pinterest-p"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<script src="assets/js/jquery-2.2.3.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/jquery.easing.1.3.js"></script>
+	<script src="assets/js/jquery.waypoints.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<script src="assets/js/jquery.counterup.min.js"></script>
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/swiper.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC5Dn5gMddkeA51OOOCX83zqquUY1rnzZU"></script>
+	<script src="assets/js/map.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+
+<!-- Mirrored from www.realagencyhtml.iconictheme.com/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 13 Aug 2018 12:40:09 GMT -->
+</html>
---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<!-- Mirrored from www.realagencyhtml.iconictheme.com/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 13 Aug 2018 12:39:12 GMT -->
+<head>
+	<meta charset="UTF-8">
+	<title>RealAgency</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="assets/images/fevicon.ico" type="image/x-icon" /> 
+    <link rel="stylesheet" href="assets/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+</head>
+<body data-spy="scroll" data-target=".navbar-nav">
+	<header class="header" id="home">
+		<nav class="navbar navbar-expand-lg" id="fixed-nav">
+			<div class="fixed-top" style="text-align:center;background:#288FEB;width:100%; position:fixed;max-height: 26px;">
+		          <div class="container">
+		            <p style="text-align:center; font-size:18px!important"><a style="color:#fff" href="tel:x-xxxx-xxx-xx"><i class="fa fa-phone" style="color:white;"></i> x (xxx) xxx-xxx</a></p>
+		          </div>
+        	</div>
+		  	<div class="container">
+	      		<a class="site-logo navbar-brand" href="#">
+			    	<h3>xyz</h3>
+			    </a>
+			    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".collapse.navbar-collapse"  aria-expanded="false" aria-label="Toggle navigation"> -->
+			    <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#main-nav-collapse" aria-controls="main-nav-collapse" aria-expanded="false" aria-label="Toggle navigation">
+			        <span class="menu-toggle">
+                        <span class="hamburger">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                        <span class="hamburger-cross">
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </span>
+			    </button>
+		    	<div class="collapse navbar-collapse justify-content-lg-end order-2 order-lg-1" id="main-nav-collapse" style="padding-top: 8px;">
+		      		<ul class="navbar-nav">
+		        		<li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+		        		<li class="nav-item"><a class="nav-link" href="index.php#about">About</a></li>
+		        		<li class="nav-item"><a class="nav-link" href="#service">Service</a></li>
+		        		<li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+		        		<li class="nav-item"><a class="nav-link" href="#blog">Blog</a></li>
+		        		<li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+		      		</ul>
+		    	</div><!-- /.navbar-collapse -->
+	    		
+		  	</div><!-- /.container-fluid -->
+		</nav>
+	</header>
